5bbc6bfb43203b906898b495642223db7bdb6f71
mviehauser <maxivichy@gmail.com>
2024-03-22 18:32:29 +0100

    added testcomment and Python interpreter

--- src/Webscraper/test_run_webscraper.py
+++ src/Webscraper/test_run_webscraper.py
@@ -2,9 +2,8 @@
 
 # In diesem Programm sollen die Funktionen aus den Modulen importiert werden, um (Test-) Durchläufe zu starten
 # Dabei sollen keine weiteren Funktionen neben run_webscraper() implementiert werden
-
+# Kommentar zum Testen der Git Funktion - mviehauser
 def run_webscraper():
-    #...
 
-if __name__ == '__main__':
-    run_webscraper()
+    if __name__ == '__main__':
+        run_webscraper()
