4ae884ed3afe2b3e2d26a1f4b9da88cfa93348f7
ppotsch <164188891+ppotsch@users.noreply.github.com>
2024-03-23 17:51:23 +0100

    important functions and constants for downloading all pdf files on the website

---
+++ src/Webscraper/download_helpers.py
@@ -0,0 +1,68 @@
+import requests
+from constants import cfsre_url, headers
+
+from bs4 import BeautifulSoup
+import re
+
+from os import remove
+from os.path import exists
+
+
+
+"""
+Downloads a PDF from the Web
+
+'local_filename = url.split('/')[-1]' : file is named after the last part in the url
+
+'headers = headers' : provides a new User-Agent to bypass bot-detection (see constants)
+"""
+def download_pdf(pdf_url):
+    local_filename = pdf_url.split('/')[-1]
+
+    with requests.get(pdf_url, headers = headers) as request:
+        with open(local_filename, 'wb') as new_file:
+            new_file.write(request.content)
+    
+    return local_filename
+
+
+"""
+All relevant pdf urls follow this RegEx: /images/monographs/ at the start, .pdf at the end
+
+
+"""
+def create_list_urls():
+    page_to_scrape = requests.get(cfsre_url, headers = headers)
+
+    soup = BeautifulSoup(page_to_scrape.text, features = "html.parser")
+
+    links = [a['href'] for a in soup.find_all('a',href=True)]
+    
+    return links
+    #An die passenden Links muss von vorne auch noch "https://www.cfsre.org" angehänt werden
+
+
+"""
+Recieves a list of Strings, containing all links on the website.
+Returns a list of Strings, with NPS pdf-links
+
+Noch in Arbeit
+"""
+def filter_list(links):
+    filtered_list = []
+    # temporary for testing:
+    for link in links:
+        if ("/images/monographs" in link) and (".pdf" in link):
+            filtered_list +=["https://www.cfsre.org" + link.strip()]
+            print("https://www.cfsre.org" + link.strip())
+    
+    return filtered_list
+
+def delete_file(filename):
+    if exists(filename):
+        remove(filename)
+
+#Test
+print(len(filter_list(create_list_urls())))
+# liefert 154 pdf Elemente
+# Maxi wieviel passende sind auf der website?
