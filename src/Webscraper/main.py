b51db41b99eae000ef44758b6b8e3cd99293f55a
mviehauser <maximilian.viehauser@st.oth-regensburg.de>
2024-04-17 10:00:33 +0200

    Name change regarding pdf_downloader

--- src/Webscraper/main.py
+++ src/Webscraper/main.py
@@ -1,52 +1,29 @@
-import json
-import os
-import pdfplumber
+from pdf_downloader import *
 
-"""
-Extracts tables from a PDF file using pdfplumber library.
+# In diesem Programm sollen die Funktionen aus den Modulen importiert werden, um (Test-) Durchläufe zu starten
+# Dabei sollen keine weiteren Funktionen neben run_webscraper() implementiert werden
 
-Args:
-- pdf_path (str): Path to the PDF file.
-- page_number (int): Page number to extract the table from. Default is 0 (first page)
+# Hinweis: VSCode gibt mir Fehlermeldungen bezüglich den importierten Funktionen, es funktioniert aber wenn man den Ordner "Webscraper" herunterläd 
 
-Returns:
-- list of lists: The extracted table.
-"""
-def extract_table_from_pdf(pdf_path, page_number=0):
+def run_webscraper():
+    links = create_list_urls()
 
-    with pdfplumber.open(pdf_path) as pdf:
-        page = pdf.pages[page_number]
-        table = page.extract_table()
-    return table
+    # For testing:
+    for link in links:
+        print(link)
+    print("\n")
 
-"""
-Saves table data as a JSON file.
+    for link in links:
+        download_pdf(link)
+        print("Downloaded: " + link)
 
-Args:
-- table (list of lists): The table data to save.
-- json_file (str): Path to the JSON file.
-"""
-def save_table_as_json(table, json_file):
+        local_pdf_filename = link.split('/')[-1]
+        # Missing : Exctract information into .json file
 
-    with open(json_file, 'w') as f:
-        json.dump(table, f)
+        delete_file(local_pdf_filename)
+        print("Deleted: " + local_pdf_filename)
+    
 
 
-# Example usage: "sample.pdf" file (sample file from url: https://www.cfsre.org/nps-discovery/monographs)
-pdf_path = 'sample.pdf'
-
-# Name of the created JSON-file
-json_filename = 'sample_table_data.json'
-
-# Relative Path to JSON-files folder
-directory = os.path.join(os.path.dirname(__file__), '..', 'JSON-files')
-json_file_path = os.path.join(directory, json_filename)
-
-
-table = extract_table_from_pdf(pdf_path)
-save_table_as_json(table, json_file_path)
-print("Table data saved as JSON successfully.")
-
-
-
-
+if __name__ == '__main__':
+        run_webscraper()
