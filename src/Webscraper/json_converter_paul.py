3728e9ed1afd04ed630b9c4c91619189042756a9
ppotsch <paul.potsch@oth-regensburg.de>
2024-04-20 13:39:17 +0200

    exctract_data_from_pdf() now  works with all 4 sample pdfs

--- src/Webscraper/json_converter_paul.py
+++ src/Webscraper/json_converter_paul.py
@@ -6,43 +6,67 @@ from rdkit.Chem.inchi import InchiToInchiKey
 from re import compile, search, DOTALL
 
 """
-There are 2 different types of pdfs:
-    Type 1 : https://www.cfsre.org/images/monographs/Medetomidine-New-Drug-Monograph-NPS-Discovery-112723.pdf
-    Type 2 : https://www.cfsre.org/images/monographs/BZO-POXIZID_101921_CFSRE_Chemistry_Report.pdf 
+Args:
+- pdf-Path
+- pdf_info: a dict which contains the key 'pdfPage' to decide which page to extract and increment the page number for possibly needed future extractions
+"""
+def extract_table_pdfPage(pdf_path, pdf_info):
+    with pdfplumber.open(pdf_path) as pdf:
+        page = pdf.pages[pdf_info['pdfPage']]
+        x= page.extract_table()
+        pdf_info['pdfPage'] += 1
+        if x is not None:
+            return x
+        else:
+            return []
+"""
+There are at least 3 different types of pdfs, with different internal structure:
+    Type 1 "NPS Discovery (new)" : https://www.cfsre.org/images/monographs/Medetomidine-New-Drug-Monograph-NPS-Discovery-112723.pdf
+    Type 2 "NPS Discovery (old)" : https://www.cfsre.org/images/monographs/BZO-POXIZID_101921_CFSRE_Chemistry_Report.pdf 
+    Type 3 "NMS LABS" : https://www.cfsre.org/images/monographs/25E-NBOH_022718_NMSLabs_Report.pdf
 
-Type 1 has all the important information on the first page.
-Type 2 has some information in the text and on page two, therefore regular expressions are needed.
+Type 1 has all the important information on the first page in a table and definies which infos have to be in DATA
+Type 2+ does not have every information in a table, therefore regular expressions and text extraction is needed.
 """
 def extract_data_from_pdf(pdf_path):
-    table = []
-    with pdfplumber.open(pdf_path) as pdf:
-        page = pdf.pages[0]
-        table = page.extract_table()
-    
+    pdf_info = {
+        'pdfPage' : 0,
+    }
+    table = extract_table_pdfPage(pdf_path, pdf_info)
     data = DATA.copy()
     
-    if table[0][0] == "Preferred Name":
+    if len(table) == 9 and table[0][0] == "Preferred Name":
         # This is Type 1
         for column in table:
             data[column[0]] = column[1]
         return data
     else:
-        # This is Type 2
-        with pdfplumber.open(pdf_path) as pdf:
-            page = pdf.pages[1]
-            table += page.extract_table()
-        
-        data["Preferred Name"] = table[0][1]
-
-        data["Chemical Formula"] = table[2][1]
-        data["Molecular Weight"] = table[2][2]
-        data["Molecular Ion [M+]"] = table[2][3]
-        data["Exact Mass [M+H]+"] = table[2][4]
+        # This is Type 2+
+        # while-loop ensures there are at least two table-rows in 'table', which means "2.1 CHEMICAL DATA" is extracted
+        while len(table) < 2:
+            table += extract_table_pdfPage(pdf_path, pdf_info)
         
         with pdfplumber.open(pdf_path) as pdf:
             page = pdf.pages[0]
             text = page.extract_text()
 
+        if len(table) == 3:
+            # if true, this means the heading(chemical prefered name) is stored in a table, NOT in the text
+            data["Preferred Name"] = table[0][1]
+            chemical_data_row = 2
+        if len(table) == 2:
+            # in this case, the heading is stored in the 4th line of text
+            data["Preferred Name"] = text.split('\n')[3]
+            chemical_data_row = 1
+        else:
+            raise ValueError(f"Unexpected table size in {pdf_path}")
+
+        data["Chemical Formula"] = table[chemical_data_row][1]
+        data["Molecular Weight"] = table[chemical_data_row][2]
+        data["Molecular Ion [M+]"] = table[chemical_data_row][3]
+        data["Exact Mass [M+H]+"] = table[chemical_data_row][4]
+
+        # These Data-keys are only to be found by RegExps
         re_synonyms = compile(r'Synonyms:.*')
         data["Synonyms"] = re_synonyms.search(text).group(0)[10:]
 
@@ -71,7 +95,7 @@ def save_table_as_json(table, json_file):
         dump(table, f)
 
 
-pdf_path = "sample2.pdf"
+pdf_path = "C:\\Users\\Fokus\\Code Projekte VS\\ProgrammierProjekt\\src\\Webscraper\\sample4.pdf"
 
 # Name of the created JSON-file
 json_filename = 'sample_table_data.json'
