24f59d62470978e779f1021934a861fd7482cdf9
mviehauser <164186582+mviehauser@users.noreply.github.com>
2024-04-24 21:44:21 +0200

    Added frontend files via upload

---
+++ main_project_frontend2/data.js
@@ -0,0 +1,25 @@
+const Data =
+    [
+    {
+        "Preferred Name": "ortho-Methylfentanyl",
+        "Synonyms": "o-Methylfentanyl, ortho-Methyl Fentanyl",
+        "Formal Name": "N-(o-tolyl)-N-[1-(2-phenylethyl)-4-piperidyl]propanamide",
+        "InChI Key": "DPAJFOSXYXNYMA-UHFFFAOYSA-N",
+        "CAS Number": "1443-53-4",
+        "Chemical Formula": "C23H30N2O",
+        "Molecular Weight": "350.50",
+        "Molecular Ion [M+]": "350",
+        "Exact Mass [M+H]+": "351.2431"
+    },
+    {
+        "Preferred Name": "N-Desethyl Etonitazene",
+        "Synonyms": "Not Applicable",
+        "Formal Name": "2-[2-[(4-ethoxyphenyl)methyl]-5-nitro-benzimidazol-1-yl]-N-ethyl-ethanamine",
+        "InChI Key": "RESPFUMJVJRUMB-UHFFFAOYSA-N",
+        "CAS Number": "2732926-26-8",
+        "Chemical Formula": "C20H24N4O3",
+        "Molecular Weight": "368.43",
+        "Molecular Ion [M+]": "368",
+        "Exact Mass [M+H]+": "369.1921"
+    }
+]
