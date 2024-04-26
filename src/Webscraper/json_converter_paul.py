ed19e9eba0bb0bed2b1b64389e13bcff9ae1e709
ppotsch <paul.potsch@oth-regensburg.de>
2024-04-26 12:19:48 +0200

    cleaner code in extract_data(), bugfixes

--- src/Webscraper/json_converter_paul.py
+++ src/Webscraper/json_converter_paul.py
@@ -9,17 +9,16 @@ from re import compile, search, DOTALL
 """
 Args:
 - pdf-Path
-- pdf_info: a dict which contains the key 'pdfPage' to decide which page to extract and increment the page number for possibly needed future extractions
+- (int) page_to_extract
 """
-def extract_table_pdfPage(pdf_path, pdf_info):
+def extract_table_text_pdfPage(pdf_path, page_to_extract):
     with pdfplumber.open(pdf_path) as pdf:
-        page = pdf.pages[pdf_info['pdfPage']]
-        x= page.extract_table()
-        pdf_info['pdfPage'] += 1
-        if x is not None:
-            return x
+        page = pdf.pages[page_to_extract]
+        text = page.extract_text()
+        if x := page.extract_table():
+            return x, text
         else:
-            return []
+            return [], text
 
 """
 There are at least 3 different types of pdfs, with different internal structure:
@@ -31,13 +30,8 @@ Type 1 has all the important information on the first page in a table and defini
 Type 2+ does not have every information in a table, therefore regular expressions and text extraction is needed.
 """
 def extract_data_from_pdf(pdf_path):
-    pdf_info = {'pdfPage' : 0}
-    table = extract_table_pdfPage(pdf_path, pdf_info)
     data = DATA.copy()
-
-    with pdfplumber.open(pdf_path) as pdf:
-        page = pdf.pages[0]
-        text = page.extract_text()
+    table, text = extract_table_text_pdfPage(pdf_path, 0)
     
     if len(table) == 9:
         # This is Type 1
@@ -60,13 +54,10 @@ def extract_data_from_pdf(pdf_path):
         return data
     
     # This is Type 2+
-    # while-loop ensures there are at least two table-rows in 'table', which means "2.1 CHEMICAL DATA" is extracted
-    while len(table) < 2:
-        table += extract_table_pdfPage(pdf_path, pdf_info)
-        
-    with pdfplumber.open(pdf_path) as pdf:
-            page = pdf.pages[1]
-            text += page.extract_text()
+    # Information(text and table) of page 2 is also needed
+    next_page_table, next_page_text = extract_table_text_pdfPage(pdf_path, 1)
+    table += next_page_table
+    text += next_page_text
 
     # Preferred Name in text
     t = text.split('\n')
@@ -81,14 +72,14 @@ def extract_data_from_pdf(pdf_path):
         data["names"] = [t[0]]
     
     # These Data-keys are only to be found in the text by RegExps
-    # DOTALL used when information can be spread over multiple lines
+    # DOTALL used, when the RegEx can be spread over multiple lines
     re_synonyms = compile(r'(?<=Synonyms:.).*?(?=Source|Important)', DOTALL)
     data["names"] += re_synonyms.search(text).group(0).replace("\n", "").split(", ")
 
     re_IUPAC_Name = compile(r'(?<=IUPAC.Name:.).*?(?=InChI)', DOTALL)
     data["iupac_name"] = re_IUPAC_Name.search(text).group(0).replace("\n", "")
 
-    re_inchiString = compile(r'(?<=InChI.String:.).*?(?=CFR)', DOTALL)
+    re_inchiString = compile(r'(?<=InChI.String:.).*?(?=CFR| )', DOTALL)
     data["inchi"] = re_inchiString.search(text).group(0).replace("\n", "")
     data["inchi_key"] = InchiToInchiKey(data["inchi"])
 
@@ -111,7 +102,7 @@ def extract_data_from_pdf(pdf_path):
         if len(table[-1]) == 5:
             data["molecular_ion_[m+]"] = table[-1][3]
         elif len(table[-1]) == 4:
-            # In this case, "Molecular Ion [M+]" is not given and me make a good guess based on the "Molecular Weight"
+            # In this case, "Molecular Ion [M+]" is not given and we make a good guess based on the "Molecular Weight"
             data["molecular_ion_[m+]"] = int(float(data["molecular_mass"]))
         else:
             print("Error: Unexpected size of table[-1]")
@@ -154,7 +145,9 @@ def format_names(data):
     for x in ['Not Available', 'None Available', 'Not Applicable']:
         if x in data["names"][1:]:
             data["names"].remove(x)
+
 """
+Edits data["formula"] by removing '\n' and combining the two lines
 # example: from 'C H N O\n20 19 3 2' to 'C20H19N3O2'
 """
 def format_formula(data):
@@ -192,7 +185,7 @@ def save_table_as_json(table, json_file):
 
 #test
 if __name__ == "__main__":
-    pdf_path = "src\\Webscraper\\pdf samples\\NN-Diethyl_Hexedrone_031819_NMSLabs_Report.pdf"
+    pdf_path = "src\\Webscraper\\pdf samples\\FluoroFuranylfentanyl_012319_ToxicologyAnalyticalReport.pdf"
 
     # Name of the created JSON-file
     #json_filename = 'sample_table_data.json'
